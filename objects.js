5240794cd143c82a3eb2fc6f3c408c6e2a27839b
monasingh27 <mona.singh@workscripts.in>
2016-04-19 18:08:46 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,13 @@
+var person={
+	firstName: 'Bob',
+	lastName: 'Martin',
+	age: 25,
+	gender: 'male'
+	
+};
+
+	console.log(person);
+
+	console.log('hello '+person.firstName+' '+person.lastName);
+	console.log('Age of Bob is: '+person.age);
+	console.log('Gender of Bob is: '+person.gender);
